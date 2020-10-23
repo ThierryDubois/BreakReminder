3b690239818d58104825eda90b216abf3f705ab0
Thierry Dubois <thierry.dubois@polymtl.ca>
2020-10-23 18:42:37 -0400

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace BreakReminder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+        }
+
+        private void ButtonShowToast_Click(object sender, RoutedEventArgs e)
+        {
+            string title = "The current time is";
+
+            string toastXmlString =
+            $@"<toast><visual>
+            <binding template='ToastGeneric'>
+            <text>{title}</text>
+            </binding>
+        </visual></toast>";
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(toastXmlString);
+
+            var toastNotification = new ToastNotification(xmlDoc);
+
+            var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            toastNotifier.Show(toastNotification);
+        }
+    }
+}
